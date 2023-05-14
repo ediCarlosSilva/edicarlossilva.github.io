bea866bd3988b1d922510bcbb6285982a5533a01
Edi Carlos <edi_csilva@hotmail.com>
2023-05-14 11:05:14 -0300

    ajuste exibição em celular

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -9,13 +9,16 @@ export default function Menu() {
                     <Link to="/">
                         <h1 className="menu__title--edi">Edi Carlos</h1>
                     </Link>
+                    <Link to="/">
+                        <h1 className="menu__title--sobremim">Sobre mim</h1>
+                    </Link>
                     <Link to="/Projetos">
                         <h1 className="menu__title--projetos">Projetos</h1>
                     </Link>
                 </header>
                 <nav>
                     <ul className="menu__list">
-                        <li className="menu__list__item"><Link to="/SobreMim">Sobre mim</Link></li>
+                        <li className="menu__list__item sobremim"><Link to="/SobreMim">Sobre mim</Link></li>
                         <li className="menu__list__item"><Link to="/Skills">Skills</Link></li>
                         <li className="menu__list__item"><Link to="/Hobbies">Hobbies</Link></li>
                         <li className="menu__list__item"><Link to="/Formacao">Formações e Cursos</Link></li>
