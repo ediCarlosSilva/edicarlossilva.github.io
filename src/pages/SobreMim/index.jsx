878b4a96974d973c57e65d43adef2bda4871e4fb
Edi Carlos <edi_csilva@hotmail.com>
2023-05-15 16:52:04 -0300

    ajuste para exibição de celular

--- src/pages/SobreMim/index.jsx
+++ src/pages/SobreMim/index.jsx
@@ -5,19 +5,12 @@ export default function SobreMim() {
     <section className="about" id="sobremim">
       <div className="about__container">
         <h3 className="about__title">Sobre mim</h3>
-        <p className="about__paragraph">Gosto de aprender sobre diversas áreas, embora algumas sejam mais
-          detalhadas e frequentes do que outras. Acredito que é válido usar esse conhecimento adquirido para
-          ajudar não só aqueles que estão próximos a mim, mas também os irmãos que cruzam meu caminho nesta
-          terra. Tenho uma família linda, com uma esposa e quatro filhos, sendo um já no céu, e tudo que faço
-          é em busca de abençoá-los.</p>
-        <p className="about__paragraph">Falar sobre nós mesmos pode ser uma tarefa desafiadora, mas ao me analisar,
-          percebo que sou uma pessoa que gosta de aprender sobre diversas áreas, embora algumas sejam mais
-          detalhadas e frequentes do que outras. Acredito que é válido usar esse conhecimento adquirido para
-          ajudar não só aqueles que estão próximos a mim, mas também os irmãos que cruzam meu caminho nesta
-          terra. Tenho uma família linda, com uma esposa e quatro filhos, sendo um já no céu, e tudo que faço
-          é em busca de abençoá-los.</p>
-        <p className="about__paragraph">Fico feliz por você estar aqui e desejo as mais seletas bênçãos do Senhor
-          Deus, Todo-Poderoso a você.</p>
+        <p className="about__paragraph">Olá, sou Edi Carlos, um profissional de Gerenciamento de TI com vasta experiência como Gerente de TI em uma rede de supermercados. Tenho um sólido conhecimento em suporte de equipes no ERP terceirizado, garantindo o bom funcionamento e a eficiência do sistema. Além disso, sou especialista em Linux, possuindo a certificação Linux Essentials, o que me permite realizar configurações nos periféricos dos PDVs que operam no sistema operacional Linux.</p>
+        <p className="about__paragraph">Minha jornada profissional tem sido marcada por um constante desejo de crescimento e ampliação de habilidades. Além do conhecimento em Linux, também possuo várias certificações MTA (Microsoft Technology Associate) da Microsoft, que validam minha competência em fundamentos essenciais da área de TI.</p>
+        <p className="about__paragraph">Como Gerente de TI, adquiri uma profunda compreensão da importância da eficiência e da confiabilidade dos sistemas de informação. Agora, estou buscando uma nova oportunidade como profissional de front-end, onde posso combinar minha experiência em gestão de TI com minhas habilidades e paixão pelo desenvolvimento web.</p>
+        <p className="about__paragraph">Tenho projetos pessoais que demonstram minha proficiência em HTML, CSS e JavaScript, as principais tecnologias do desenvolvimento front-end. Esses projetos me permitiram criar interfaces atraentes e funcionais. Além disso, sou experiente no uso do Git para controle de versionamento de código, o que me torna apto a colaborar efetivamente em projetos em equipe.</p>
+        <p className="about__paragraph">Para aprimorar ainda mais minhas habilidades, tenho me dedicado ao estudo e ao desenvolvimento de projetos utilizando o React, uma biblioteca JavaScript amplamente utilizada para criar interfaces de usuário dinâmicas. Atualmente, estou participando do projeto ONE da Alura em parceria com a Oracle, onde tenho resolvido desafios e aprimorado meu conhecimento em desenvolvimento front-end.</p>
+        <p className="about__paragraph">Estou confiante em minha capacidade de enfrentar desafios, aprender rapidamente e entregar resultados excepcionais.</p>
       </div>
     </section>
   )
