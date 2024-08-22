865cf95898cb967e2d85c5085b7946626c41a489
Edi Carlos <edi.csilva.dev@outlook.com>
2024-08-22 10:21:36 -0300

    added resume update

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,13 +1,13 @@
-import './Footer.css';
-import { i18n } from '../../translate/i18n';
+import "./Footer.css";
+import { i18n } from "../../translate/i18n";
 
 export default function Footer() {
-    return (
-        <section className="footer">
-        <div className="footer__rodape">
-          <h2>{i18n.t('footer.desenvolvido')} Edi Carlos</h2>
-          <p>2023</p>
-        </div>
-      </section>
-    )
+  return (
+    <section className="footer">
+      <div className="footer__rodape">
+        <h2>{i18n.t("footer.desenvolvido")} Edi Carlos</h2>
+        <p>2024</p>
+      </div>
+    </section>
+  );
 }
--- src/translate/languages/pt.js
+++ src/translate/languages/pt.js
@@ -1,75 +1,75 @@
 const messages = {
-    pt: {
-        translations: {
-            menu: {
-                sobremim: "Sobre mim",
-                skills: "Skills",
-                hobbies: "Hobbies",
-                formacao: "Formação",
-                experiencia: "Experiencia",
-                projetos: "Projetos"
-            },
-            footer: {
-                desenvolvido: "Desenvolvido por"
-            },
-            titlePage: {
-                    title: "Home | Portifólio",
-                title__bio__ola: "Olá, meu nome é ",
-                title__bio__edi: "Edi Carlos",
-                title__bio__sou: "Sou ",
-                title__bio__analista: "Analista de Sistemas",
-                title__bio__skills: " com skills de ",
-                title__bio__frontend: "front-end developer",
-                title__bio__other: " and other things",
-                title__link__resume: "./edi-carlos-resume.pdf"
-            },
-            sobremim: {
-                title: "Sobre Mim | Portifólio",
-                titulo: "Sobre mim",
-                p1: "Sou Edi Carlos, Analista e Desenvolvedor de Sistemas formado pela Fatec Sorocaba. Atualmente, desempenho o papel de Tech Lead em Infraestrutura em um supermercado, onde sou responsável por gerenciar a infraestrutura de TI e fornecer suporte aos colaboradores.",
-                p2: "Ao longo do tempo, tenho desenvolvido projetos através de estudos e cursos, buscando constantemente me aprimorar. Tenho focado em todas as tecnologias relacionadas ao desenvolvimento web, ampliando meu conhecimento tanto no front-end quanto no back-end.",
-                p3: "Além disso, mantenho-me atualizado em infraestrutura, abrangendo áreas como sistema operacional Linux e Windows Server, bancos de dados, redes e segurança da informação. Reconheço a importância dessas habilidades para fornecer um ambiente seguro e estável para as aplicações.",
-                p4: "Busco constantemente aprimorar meu conhecimento em ferramentas de gestão e análise de dados, como planilhas e consultas a bancos de dados. Isso me permite tomar decisões fundamentadas com base em dados e oferecer soluções eficazes para os desafios enfrentados.",
-                p5: "Atualmente, estou embarcando em uma transição para me tornar um Desenvolvedor Front-end. Estou comprometido em expandir minhas habilidades em linguagens de programação, frameworks e tecnologias relacionadas. Meu objetivo é ser capaz de criar aplicações web completas e robustas, proporcionando uma experiência de usuário excepcional.",
-                p6: "Sou um profissional dedicado e proativo, sempre em busca de aprendizado contínuo e crescimento profissional. Com minha sólida base em infraestrutura e minha paixão pelo desenvolvimento web, estou pronto para enfrentar novos desafios e contribuir de forma significativa para projetos de software."
-            },
-            hobbies: {
-                title: "Hobbies | Portifólio",
-                ouvir: "Ouvir Música",
-                cantar: "Cantar",
-                ler: "Ler",
-                oracao: "Oração"
-            },
-            formacao: {
-                title: "Formação Acadêmica | Portifólio",
-                titulo: "Formação Acadêmica",
-                formacao1: "Análise e Desenvolvimento de Sistemas",
-                formacao2: "R2 - Matemática",
-                formacao3: {curso: "Ciências Contábeis", conclusao: "Cursando"}
-            },
-            experiencia: {
-                title: "Experiência Profissional | Portifólio",
-                titulo: "Experiência Profissional",
-                empresa: {
-                    nome: "Malucho Supermercados",
-                    funcao: "Tech Lead",
-                    inicio: 2003,
-                    fim: "Atual"
-                }
-            },
-            tiposProjeto: {
-                estudo: "Estudo",
-                challenge: "Challenge",
-                testeTecnico: "Teste Técnico",
-                voluntario: "Voluntário"
-            },
-            explicacaoOrigem: {
-                "Alura": "Plataforma de cursos de Tecnologia.",
-                "Frontendmentor": "Site com desafios de frontend.",
-                "Tech Recruit": "Aplicação de teste por tech recruiter."
-            }
-        }
-    }
-}
+  pt: {
+    translations: {
+      menu: {
+        sobremim: "Sobre mim",
+        skills: "Skills",
+        hobbies: "Hobbies",
+        formacao: "Formação",
+        experiencia: "Experiencia",
+        projetos: "Projetos",
+      },
+      footer: {
+        desenvolvido: "Desenvolvido por",
+      },
+      titlePage: {
+        title: "Home | Portifólio",
+        title__bio__ola: "Olá, meu nome é ",
+        title__bio__edi: "Edi Carlos",
+        title__bio__sou: "Sou ",
+        title__bio__analista: "Analista de Sistemas",
+        title__bio__skills: " com skills de ",
+        title__bio__frontend: "front-end developer",
+        title__bio__other: " and other things",
+        title__link__resume: "./edi-carlos-resume.pdf",
+      },
+      sobremim: {
+        title: "Sobre Mim | Portifólio",
+        titulo: "Sobre mim",
+        p1: "Sou Edi Carlos, Analista e Desenvolvedor de Sistemas formado pela Fatec Sorocaba. Atualmente, desempenho o papel de gerente de TI em um supermercado, onde sou responsável por gerenciar a infraestrutura de TI e fornecer suporte aos colaboradores.",
+        p2: "Ao longo do tempo, tenho desenvolvido projetos através de estudos e cursos, buscando constantemente me aprimorar. Tenho focado em todas as tecnologias relacionadas ao desenvolvimento web, ampliando meu conhecimento tanto no front-end quanto no back-end.",
+        p3: "Além disso, mantenho-me atualizado em infraestrutura, abrangendo áreas como sistema operacional Linux e Windows Server, bancos de dados, redes e segurança da informação. Reconheço a importância dessas habilidades para fornecer um ambiente seguro e estável para as aplicações.",
+        p4: "Busco constantemente aprimorar meu conhecimento em ferramentas de gestão e análise de dados, como planilhas e consultas a bancos de dados. Isso me permite tomar decisões fundamentadas com base em dados e oferecer soluções eficazes para os desafios enfrentados.",
+        p5: "Atualmente, estou embarcando em uma transição para me tornar um Desenvolvedor Front-end. Estou comprometido em expandir minhas habilidades em linguagens de programação, frameworks e tecnologias relacionadas. Meu objetivo é ser capaz de criar aplicações web completas e robustas, proporcionando uma experiência de usuário excepcional.",
+        p6: "Sou um profissional dedicado e proativo, sempre em busca de aprendizado contínuo e crescimento profissional. Com minha sólida base em infraestrutura e minha paixão pelo desenvolvimento web, estou pronto para enfrentar novos desafios e contribuir de forma significativa para projetos de software.",
+      },
+      hobbies: {
+        title: "Hobbies | Portifólio",
+        ouvir: "Ouvir Música",
+        cantar: "Cantar",
+        ler: "Ler",
+        oracao: "Oração",
+      },
+      formacao: {
+        title: "Formação Acadêmica | Portifólio",
+        titulo: "Formação Acadêmica",
+        formacao1: "Análise e Desenvolvimento de Sistemas",
+        formacao2: "R2 - Matemática",
+        formacao3: { curso: "Ciências Contábeis", conclusao: "Cursando" },
+      },
+      experiencia: {
+        title: "Experiência Profissional | Portifólio",
+        titulo: "Experiência Profissional",
+        empresa: {
+          nome: "Malucho Supermercados",
+          funcao: "Tech Lead",
+          inicio: 2003,
+          fim: "Atual",
+        },
+      },
+      tiposProjeto: {
+        estudo: "Estudo",
+        challenge: "Challenge",
+        testeTecnico: "Teste Técnico",
+        voluntario: "Voluntário",
+      },
+      explicacaoOrigem: {
+        Alura: "Plataforma de cursos de Tecnologia.",
+        Frontendmentor: "Site com desafios de frontend.",
+        "Tech Recruit": "Aplicação de teste por tech recruiter.",
+      },
+    },
+  },
+};
 
-export { messages }
+export { messages };
